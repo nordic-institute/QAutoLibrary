a7d65a4677c7cc573d63c5118d43766186344238
Lasse.Matikainen <lasse.matikainen@qautomate.fi>
2018-05-27 09:14:28 +0300

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="QAutoLibrary",
-    version="0.0.1",
+    version="0.0.2",
     author="QAutomate",
     author_email="contact@qautomate.fi",
     description="",
